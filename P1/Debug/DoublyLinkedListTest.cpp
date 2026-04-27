ab5fc417a68971a232dd5b1ee690a7b690785b88
mei-emilia <mei.inokuma@gmail.com>
2026-04-27 22:44:35 +0200

    new test cases

--- P1/Debug/DoublyLinkedListTest.cpp
+++ P1/Debug/DoublyLinkedListTest.cpp
@@ -258,3 +258,158 @@ TEST_CASE("Vertauschen von zwei Elementen - Zwei Knoten aus der Mitte, nebeneina
     REQUIRE(originalAddressFirstNode == newAddressFirstNode);
     REQUIRE(originalAddressSecondNode == newAddressSecondNode);
 }
+
+TEST_CASE("Vertauschen von zwei Elementen - Erster Knoten mit direktem Nachbar")
+{
+    DoublyLinkedList* list = new DoublyLinkedList();
+
+
+    list->addNode(4);
+    list->addNode(9);
+    list->addNode(7);
+    list->addNode(5);
+    list->addNode(3);
+    list->addNode(2);                                                                                                             
+    list->addNode(8);
+    list->addNode(1);
+
+    REQUIRE(list->getNodesCount() == 8);
+    Node* tmp = list->getAnchor();
+    int pos = 0;
+    string originalAddressFirstNode = findAddressOfPosition(list, pos);
+    for (int i = 0; i < 2; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string originalAddressSecondNode = findAddressOfPosition(list, pos);
+
+    REQUIRE(list->swap(4, 9) == true);
+    tmp = list->getAnchor();
+    pos = 0;
+    string newAddressSecondNode = findAddressOfPosition(list, pos);
+    for (int i = 0; i < 2; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string newAddressFirstNode = findAddressOfPosition(list, pos);
+
+    REQUIRE(originalAddressFirstNode == newAddressFirstNode);
+    REQUIRE(originalAddressSecondNode == newAddressSecondNode);
+}
+
+TEST_CASE("Vertauschen von zwei Elementen - Letzer Knoten mit direktem Nachbar")
+{
+    DoublyLinkedList* list = new DoublyLinkedList();
+
+
+    list->addNode(4);
+    list->addNode(9);
+    list->addNode(7);
+    list->addNode(5);
+    list->addNode(3);
+    list->addNode(2);                                                                                                             
+    list->addNode(8);
+    list->addNode(1);
+
+    REQUIRE(list->getNodesCount() == 8);
+    Node* tmp = list->getAnchor();
+    int pos = 0;
+    for (int i = 0; i < 7; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string originalAddressFirstNode = findAddressOfPosition(list, pos);
+    for (int i = 0; i < 8; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string originalAddressSecondNode = findAddressOfPosition(list, pos);
+
+    REQUIRE(list->swap(8, 1) == true);
+    tmp = list->getAnchor();
+    pos = 0;
+    for (int i = 0; i < 7; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string newAddressSecondNode = findAddressOfPosition(list, pos);
+    for (int i = 0; i < 8; i++){
+        pos = i;
+        if(tmp->getNext())
+            tmp = tmp->getNext();
+    }
+    string newAddressFirstNode = findAddressOfPosition(list, pos);
+
+    REQUIRE(originalAddressFirstNode == newAddressFirstNode);
+    REQUIRE(originalAddressSecondNode == newAddressSecondNode);
+}
+
+TEST_CASE("Vertauschen von zwei Elementen - Nur zwei Elemente in der Liste")
+{
+    DoublyLinkedList* list = new DoublyLinkedList();
+
+    list->addNode(4);
+    list->addNode(9);
+
+    REQUIRE(list->getNodesCount() == 2);
+    string originalAddressFirstNode = findAddressOfPosition(list, 0);
+    string originalAddressSecondNode = findAddressOfPosition(list, 1);
+
+    REQUIRE(list->swap(4, 9) == true);
+    string newAddressSecondNode = findAddressOfPosition(list, 0);
+    string newAddressFirstNode = findAddressOfPosition(list, 1);
+
+    REQUIRE(originalAddressFirstNode == newAddressFirstNode);
+    REQUIRE(originalAddressSecondNode == newAddressSecondNode);
+}
+
+
+TEST_CASE("Tauschen eines Elements mit einem Neuen")
+{
+
+    DoublyLinkedList* list = new DoublyLinkedList();
+    list->addNode(2);
+    list->addNode(5);
+    list->addNode(6);
+    list->addNode(56);
+    list->addNode(4);
+
+    //Replacing a Middle Node
+    string originalAddress = findAddress(list, 6);
+    list->replace(6, 3);
+    string newAddress = findAddress(list, 3);
+    REQUIRE(originalAddress != newAddress);
+
+    //Replacing the First Node
+    originalAddress = findAddress(list, 2);
+    list->replace(2, 12);
+    newAddress = findAddress(list, 12);
+    REQUIRE(originalAddress != newAddress);
+
+    list->addNode(101);
+
+    //Replacing the Last Node
+    originalAddress = findAddress(list, 101);
+    list->replace(101, 10);
+    newAddress = findAddress(list, 10);
+    REQUIRE(originalAddress != newAddress);
+
+    //Replacing an already swapped Node
+    originalAddress = findAddress(list, 3);
+    list->replace(3, 21);
+    newAddress = findAddress(list, 21);
+    REQUIRE(originalAddress != newAddress);
+
+    //Trying to replace node with an existing value, nothing should happen
+    originalAddress = findAddress(list, 5);
+    list->replace(5, 21);
+    newAddress = findAddress(list, 21);
+    REQUIRE(originalAddress != newAddress);
+    
+    REQUIRE(list->getNodesCount() == 6);
+}
